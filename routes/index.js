6d8165d0e0e39e58886b30695af20a68241e8a4d
Marrione Nguyen <mtiamsic@gmail.com>
2020-11-16 17:07:17 -0800

    create api routes for pizza

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+cont apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
